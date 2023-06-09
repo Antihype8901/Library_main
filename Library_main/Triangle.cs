2b489a79d9ba2d9920c02b18f48fbbce8e2ab5bb
Antihype8901 <matvey.petukhov.03@bk.ru>
2023-06-10 01:41:50 +0500

    Добавьте файлы проекта.

---
+++ Library_main/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_main
+{
+    public class Triangle : Shape
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public override double CalculateArea()
+        {
+            double s = (A + B + C) / 2;
+            return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = new double[] { A, B, C };
+            Array.Sort(sides);
+            return Math.Abs(Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) - Math.Pow(sides[2], 2)) < 1e-10;
+        }
+    }
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Library_main;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using Microsoft.Analytics.UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void CircleAreaTest()
+        {
+            Circle circle = new Circle(5);
+            double expectedArea = Math.PI * Math.Pow(5, 2);
+            Assert.AreEqual(expectedArea, circle.CalculateArea(), 2);
+        }
+
+        [TestMethod]
+        public void TriangleAreaTest()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            double expectedArea = 6;
+            Assert.AreEqual(expectedArea, triangle.CalculateArea(), 2);
+        }
+
+        [TestMethod]
+        public void TriangleIsRightTest()
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+            Assert.IsTrue(triangle.IsRightTriangle());
+        }
+
+    }
+}
